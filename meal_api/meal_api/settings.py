251ba243fcc28aa86b626e9ec16ec31fc29a46e0
icarrera <irismcarrera@gmail.com>
2016-06-14 12:52:12 -0700

    Changed DB to postgreSQL. Added rest framework to installed apps.

--- meal_api/meal_api/settings.py
+++ meal_api/meal_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'locations_model',
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'meal_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meal-test',
+        'USER': 'roboiris',
+        'PASSWORD': 'secret',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
