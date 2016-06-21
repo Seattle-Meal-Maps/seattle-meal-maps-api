fd5939642b6f4804bef6a80eed42b56f52f5dc65
icarrera <irismcarrera@gmail.com>
2016-06-20 21:31:31 -0700

    removed tics around DATABASES

--- meal_api/meal_api/settings.py
+++ meal_api/meal_api/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'meal_api.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 
-# `DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'meal-test',
-#         'USER': 'roboiris',
-#         'PASSWORD': 'secret',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }`
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meal-test',
+        'USER': 'roboiris',
+        'PASSWORD': 'secret',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
